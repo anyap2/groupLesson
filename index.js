35988ae283a9815c63ca93e74c572314c8721d6a
anyap2 <pashover.anya20114@gmail.com>
2023-01-09 11:58:17 +0200

    Anya's addition

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import { useState } from 'react'
+
 console.log("mark")
+console.log("Asaf_2")
+ 
+const [situation, setSituation]=useState("Hungry Mark")
+
+setSituation("Well-fed Mark")
+console.log(situation)
